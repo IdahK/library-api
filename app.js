e7137ec93c116b9b1b741ccf7a6fe661160b400f
Idah <idah.koome@strathmore.edu>
2020-08-25 00:39:48 +0300

    Render static files via public folder and instaled modules

--- app.js
+++ app.js
@@ -5,12 +5,22 @@ var chalk = require('chalk');
 var debug = require('debug');
 var morgan = require('morgan');
 
+//handles directory pathnames irrespective of platform
+var path = require('path');
+
 var app = express();
 
 app.use(morgan('tiny'));
+//setting up a static directory for static files
+app.use(express.static(path.join(__dirname, '/public/')));
+// for css files also check here if not in public folder
+app.use('/css',express.static(path.join(__dirname,'/node_modules/bootstrap/dist/css')));
+//for js and jquery files check here if not in public folder
+app.use('/js',express.static(path.join(__dirname,'/node_modules/bootstrap/dist/js')));
+app.use('/js',express.static(path.join(__dirname,'/node_modules/bootstrap/jquery/dist')));
 
 app.get('/', function(req, res){
-    res.send('Hello from my library app');
+    res.sendFile(path.join(__dirname ,'views/index.html'));
 });
 
 app.listen(3000, function(){
