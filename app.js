c92004509a186cc60724f8870e7a60784b9f414b
Idah <idah.koome@strathmore.edu>
2020-08-24 23:46:35 +0300

    Setup debugging

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express = require('express');
 
+//Sets colors on error messages
+var chalk = require('chalk');
+var debug = require('debug');
+var morgan = require('morgan');
+
 var app = express();
+
+app.use(morgan('tiny'));
 
 app.get('/', function(req, res){
     res.send('Hello from my library app');
 });
 
 app.listen(3000, function(){
-    console.log('Listening on port 3000');
+    debug(`Listening on port' ${chalk.green('3000')}`); //template strings
 });
