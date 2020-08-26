dd162cb6acc3109d8f7f1820e05d314206228893
Idah <idah.koome@strathmore.edu>
2020-08-26 23:25:33 +0300

    Nodemon setup

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
-var express = require('express');
+/* eslint-disable linebreak-style */
+const express = require('express');
 
-//Sets colors on error messages
-var chalk = require('chalk');
-var debug = require('debug');
-var morgan = require('morgan');
+// Sets colors on error messages
+const chalk = require('chalk');
+const debug = require('debug');
+const morgan = require('morgan');
 
-//handles directory pathnames irrespective of platform
-var path = require('path');
+// handles directory pathnames irrespective of platform
+const path = require('path');
 
-var app = express();
+const port = process.env.PORT || 3000;
+
+const app = express();
 
 app.use(morgan('tiny'));
-//setting up a static directory for static files
-app.use(express.static(path.join(__dirname, '/public/')));
-// for css files also check here if not in public folder
-app.use('/css',express.static(path.join(__dirname,'/node_modules/bootstrap/dist/css')));
-//for js and jquery files check here if not in public folder
-app.use('/js',express.static(path.join(__dirname,'/node_modules/bootstrap/dist/js')));
-app.use('/js',express.static(path.join(__dirname,'/node_modules/jquery/dist')));
 
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname ,'views/index.html'));
+// setting up a static directory for static files
+app.use(express.static(path.join(__dirname, '/public/')));
+
+// for css files also check here if not in public folder
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+// for js and jquery files check here if not in public folder
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.listen(3000, function(){
-    debug(`Listening on port' ${chalk.green('3000')}`); //template strings
+app.listen(port, () => {
+  debug(`Listening on port' ${chalk.green(port)}`); // template strings
 });
